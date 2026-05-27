23b1acc0c47f7b7dfe3b69ab33700993e1eb716e
qossiim <qosimtoirov2000@gmail.com>
2026-05-27 18:19:03 +0900

    feat:modiyf chatting related design

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -140,8 +140,8 @@ const withLayoutBasic = (Component: any) => {
 						<Stack id={'main'}>
 							<Component {...props} />
 						</Stack>
-
-						{user?._id && <Chat />}
+{
+						user?._id && <Chat />}
 
 						<Stack id={'footer'}>
 							<Footer />
